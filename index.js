26f7ca1db94b141fdf7c044dfdc75b64a1d21f81
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2021-05-13 14:07:39 +0200

    add comments

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ const HTML = `<body>
 const $ = cheerio.load(HTML)
 const bodyContents = $('body').contents()
 
+/**
+ * Only dom elements are passed into the filter method
+ */
 const filteredItems = bodyContents.filter((i, item) => {
     console.log('filter ' + i, item.type)
     return item.type === 'text'
 })
 
+/**
+ * No items are logged, as textNodes aren't passed into the filter method, so it's not possible to filter on textNodes
+ */
 console.log('Filtered items: ')
 filteredItems.each((i, item) => console.log(item))
 
 console.log(' ')
 
+/**
+ * All dom nodes are passed into the each method
+ */
 bodyContents.each((i, item) => {
     console.log('each ' + i, item.type)
 })
